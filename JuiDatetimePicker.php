548816431056fc9e7690f0cb72c7d33ff96b6fe3
Mackiavelly <ksander.zone@gmail.com>
2015-09-23 18:12:24 +0300

    fix options

--- JuiDatetimePicker.php
+++ JuiDatetimePicker.php
@@ -31,6 +31,10 @@ class JuiDatetimePicker extends InputWidget
 
     public $clientOptions = [];
 
+    public $ignoreReadonly = false;
+
+    public $disableAlt = false;
+
     public function init()
     {
         $formatter = Yii::$app->getFormatter();
@@ -67,7 +71,10 @@ class JuiDatetimePicker extends InputWidget
         } else {
             $value = $this->value;
         }
-        $altOptions = ['id' => $altInputId];
+        $altOptions = [
+            'id' => $altInputId,
+            'name' => $altInputId,
+        ];
         if (!is_null($value) && ($value !== '')) {
             $formatter = Yii::$app->getFormatter();
             try {
@@ -77,11 +84,12 @@ class JuiDatetimePicker extends InputWidget
                 // ignore exception and keep original value if it is not a valid date
             }
         }
-        $this->options['name'] = false;
         if ($hasModel) {
-            $output = Html::activeTextInput($this->model, $this->attribute, $this->options) . Html::activeHiddenInput($this->model, $this->attribute, $altOptions);
+            $output = Html::activeTextInput($this->model, $this->attribute, $this->options)
+                .((!$this->disableAlt) ? Html::activeHiddenInput($this->model, $this->attribute, $altOptions) : null);
         } else {
-            $output = Html::textInput($this->name, $this->value, $this->options) . Html::hiddenInput($this->name, $this->value, $altOptions);
+            $output = Html::textInput($this->name, $this->value, $this->options)
+                .((!$this->disableAlt) ? Html::hiddenInput($this->name, $this->value, $altOptions) : null);
         }
         $this->clientOptions = array_merge([
             'numberOfMonths' => $this->numberOfMonths,
@@ -96,7 +104,7 @@ class JuiDatetimePicker extends InputWidget
             'altField' => '#' . $altInputId,
             'altFieldTimeOnly' => false
         ]);
-        if (array_key_exists('readonly', $this->options) && $this->options['readonly']) {
+        if (!$this->ignoreReadonly && array_key_exists('readonly', $this->options) && $this->options['readonly']) {
             $this->clientOptions['beforeShow'] = new JsExpression('function (input, inst) { return false; }');
         }
         $js = 'jQuery(\'#' . $inputId . '\').datetimepicker(' . Json::htmlEncode($this->clientOptions) . ');';
